800fafa7ea70646988ca2756b7bcf79c3ca54c88
alinlpkv <alinlpkv@gmail.com>
2024-04-04 16:20:14 +0300

    back

--- app/bot_routers.py
+++ app/bot_routers.py
@@ -17,11 +17,11 @@ router = Router()
 @router.message(Command('app'))
 async def open_app(message: types.Message) -> None:
     app_url = 'https://alinlpkv.github.io/tg_web_app/app/static/page.html'
-    markup = types.ReplyKeyboardMarkup(
-        keyboard=[
-            [types.KeyboardButton(text='open app', web_app=WebAppInfo(url=app_url))],
-        ],
+    markup = types.InlineKeyboardMarkup(
         resize_keyboard=True,
+        inline_keyboard=[
+            [types.InlineKeyboardButton(text='open app', web_app=WebAppInfo(url=app_url))],
+        ]
     )
     await message.answer('For create meeting', reply_markup=markup)
 
@@ -29,18 +29,7 @@ async def open_app(message: types.Message) -> None:
 @router.message(F.web_app_data)
 async def web_app(message: types.Message):
     data = json.loads(message.web_app_data.data)
+    print(data)
     meeting_name = data.get('meeting_name')
     await message.answer(f'Meeting "{meeting_name}" created!')
 
-
-class WebAppDataFilter(Filter):
-    async def __call__(self, message: Message, **kwargs) -> Union[bool, Dict[str, Any]]:
-        return dict(web_app_data=message.web_app_data) if message.web_app_data else False
-
-
-@router.message(WebAppDataFilter())
-async def handle_web_app_data(message: types.Message, web_app_data: types.WebAppData):
-    print(web_app_data)
-    web_app_data = json.loads(message.web_app_data.data)
-    meeting_name = web_app_data.get('meeting_name')
-    await message.answer(f'Meeting "{meeting_name}" created!')
---
+++ app/static/script.js
@@ -0,0 +1,22 @@
+const tg = window.Telegram.WebApp;
+tg.expand();
+
+
+let user_name = document.getElementById("user_name");
+user_name.innerText = `${tg.initDataUnsafe.user.first_name}`;
+
+let create = document.getElementById("create");
+create.addEventListener("click", () => {
+    let meeting_name = document.getElementById("meeting_name").value;
+    let meeting_description = document.getElementById("meeting_description").value;
+    let meeting_date = document.getElementById("meeting_date").value;
+
+    let data = {
+        meeting_name: meeting_name,
+        meeting_description: meeting_description,
+        meeting_date: meeting_date
+    };
+
+    tg.sendData(JSON.stringify(data));
+    tg.close();
+});
