27823e60c4926afdb71137eec3af44f2849417d2
alinlpkv <alinlpkv@gmail.com>
2024-04-04 15:32:12 +0300

    back

--- app/bot_routers.py
+++ app/bot_routers.py
@@ -7,25 +7,23 @@ from aiogram.fsm.state import State, StatesGroup
 from aiogram.utils.chat_action import ChatActionMiddleware
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 import json
+
 router = Router()
 
 
 @router.message(Command('app'))
 async def open_app(message: types.Message) -> None:
+    app_url = 'https://alinlpkv.github.io/tg_web_app/app/static/page.html'
     markup = types.ReplyKeyboardMarkup(
         keyboard=[
-            [types.KeyboardButton(text='open app',
-                                        web_app=WebAppInfo(
-                                            url='https://alinlpkv.github.io/tg_web_app/app/static/page.html'
-                                        )
-                                        )],
+            [types.KeyboardButton(text='open app',web_app=WebAppInfo(url=app_url))],
         ],
         resize_keyboard=True,
     )
     await message.answer('For create meeting', reply_markup=markup)
 
 
-@router.message(content_types=['web_app_data'])
+@router.message(F.web_app_data)
 async def web_app(message: types.Message):
     data = json.loads(message.web_app_data.data)
     meeting_name = data.get('meeting_name')
