07401b563214a8994a89676a0496e41c5f2b2279
alinlpkv <alinlpkv@gmail.com>
2024-04-10 14:47:08 +0300

    script

--- meeting_app/static/script.js
+++ meeting_app/static/script.js
@@ -3,11 +3,36 @@ $(document).ready(function() {
     let tg = window.Telegram.WebApp;
     tg.expand();
 
-//    if (tg.initData == '') {
-//        $('body').empty();
-//    }
+    //    if (tg.initData == '') {
+    //        $('body').empty();
+    //    }
+
+    fetchData();
 
 });
+
+
+async function fetchData() {
+    let user_id = 34229763;
+    try {
+        const response = await fetch("http://localhost:8000/meeting/" + user_id);
+        const responseData = await response.json();
+        const deleteElement = document.querySelector("#container");
+        deleteElement.innerHTML = '';
+
+        console.log(responseData)
+
+        $.each(responseData, function(index, meeting) {
+            $('#meetingTable tbody').append('<tr><td>' + meeting.theme + '</td><td>' + meeting.date_start + '</td></tr>');
+        });
+        if (responseData.length > 0) {
+            $('#noMeeting').text('');
+        };
+    } catch (error) {
+        console.error("Ошибка при получении данных:", error);
+    }
+};
+
 
 var currentTime = new Date();
 var offset = currentTime.getTimezoneOffset();
