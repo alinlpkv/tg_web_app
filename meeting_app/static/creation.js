b01ea12c972125e8fbf2c85f329a62b3edd9bd99
alinlpkv <alinlpkv@gmail.com>
2024-04-10 23:28:34 +0300

    new button

--- meeting_app/static/creation.js
+++ meeting_app/static/creation.js
@@ -1,12 +1,11 @@
+let tg = window.Telegram.WebApp;
 
-$(document).ready(function() {
-    let tg = window.Telegram.WebApp;
+//if (tg.initDataUnsafe == '') {
+//    $('body').empty();
+//}
 
-//    if (tg.initDataUnsafe == '') {
-//        $('body').empty();
-//    }
+let user_id = tg?.initDataUnsafe?.user?.id || 342297636;
 
-});
 
 $('#datetimepicker1').datetimepicker({
     locale: moment.locale('ru')
@@ -34,12 +33,12 @@ $("#datetimepicker1").on("dp.change", function (e) {
 
 function postData(user_data) {
     return new Promise(function(resolve, reject) {
-        let tg = window.Telegram.WebApp;
-//        let user_id = tg.initDataUnsafe.user.id
-        let user_id = 342297636;
-        let query = "?user_id=" + user_id + "&" + "theme=" + user_data.theme +"&" + "date_start="+ user_data.date_start + "&" + "date_end="+ user_data.date_end + "&" +"description=" + user_data.description +  "&" +"timezone=" + user_data.timezone;
+        let query = `?user_id=${user_id}&theme=${user_data.theme}&` +
+                    `date_start=${user_data.date_start}&date_end=${user_data.date_end}&` +
+                    `description=${user_data.description}&timezone=${user_data.timezone}`;
+
         console.log(query);
-        fetch("http://localhost:8020/meeting/create" + query)
+        fetch(`http://localhost:8020/meeting/create${query}`)
             .then(response => {
                 if (!response.ok) {
                     throw new Error('Ошибка при отправке данных на сервер: ' + response.statusText);
@@ -67,35 +66,28 @@ $('#create').click(function(event) {
         $('#errorMessageDateStart').text('Заполните время начала встречи');
         return;
     }
-    if ($('#theme').val() === '') {
+    if ($('#theme').val().trim() === '') {
         event.preventDefault();
         $('#errorMessageTheme').text('Заполните тему встречи');
         return;
     }
 
-    let theme = $('#theme').val();
-    let date_start = $('#date_start').val();
-    let date_end = $('#date_end').val();
-    let description = $('#description').val();
-    var currentTime = new Date();
-    var offset = currentTime.getTimezoneOffset();
-
     let meeting_data = {
-        theme: theme,
-        date_start: date_start,
-        date_end: date_end,
-        description: description,
-        timezone: offset
+        theme: $('#theme').val(),
+        date_start: $('#date_start').val(),
+        date_end: $('#date_end').val() || '',
+        description: $('#description').val() || '',
+        timezone: new Date().getTimezoneOffset()
     };
 
     postData(meeting_data).
         then(function(response) {
             console.log('Данные успешно отправлены на сервер');
             $('form')[0].reset();
+            window.location.href = 'https://alinlpkv.github.io/tg_web_app/meeting_app/templates/meeting.html';
         })
         .catch(function(error) {
             console.error('Ошибка при отправке данных на сервер:', error);
         });
 
-//    event.preventDefault();
 });
--- meeting_app/static/meeting.js
+++ meeting_app/static/meeting.js
@@ -1,41 +1,38 @@
+let tg = window.Telegram.WebApp;
+tg.expand();
+
+//if (tg.initData == '') {
+//    $('body').empty();
+//}
+
+let user_id = tg.initDataUnsafe.user?.id || 342297636;
+fetchData(user_id);
 
 
-$(document).ready(function() {
-    let tg = window.Telegram.WebApp;
-    tg.expand();
+async function fetchData(user_id) {
 
-//    if (tg.initData == '') {
-//        $('body').empty();
-//    }
-
-    let user_id = tg.initDataUnsafe.user.id
-
-    fetchData(user_id);
-
-});
-
-
-async function fetchData(id_) {
-    let user_id = id_;
     try {
-        const response = await fetch("http://localhost:8020/meeting/show/" + user_id);
+        const response = await fetch(`http://localhost:8020/meeting/show/${user_id}`);
         const responseData = await response.json();
 
-        if (responseData.length == 0) {
-            const deleteElement = document.querySelector("#container");
-            deleteElement.innerHTML = '';
-        }
-
         console.log(responseData);
-        console.log(responseData[0].theme);
 
-        $.each(responseData, function(index, meeting) {
-            $('#meetingTable tbody').append('<tr><td>' + meeting.theme + '</td><td>' + meeting.date_start + '</td></tr>');
-        });
-        if (responseData.length > 0) {
-            $('#noMeeting').text('');
+        if (responseData) {
+            if (responseData.length == 0) {
+                const deleteElement = document.querySelector("#container");
+                deleteElement.innerHTML = '';
+                return;
+            }
+
+            if (responseData.length > 0) {
+                $('#noMeeting').text('');
+
+                $.each(responseData, function(index, meeting) {
+                    $('#meetingTable tbody').
+                    append('<tr><td>' + meeting.theme + '</td><td>' + meeting.date_start + '</td></tr>');
+                });
+            };
         };
-
 
     } catch (error) {
         console.error("Ошибка при получении данных:", error);
