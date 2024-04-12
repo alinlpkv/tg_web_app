769e0c7792068f4b8c2ce29911369e07a5aad073
Lapikova Alina <49127281+alinlpkv@users.noreply.github.com>
2024-04-12 22:22:14 +0300

    Update meeting.js

--- meeting_app/static/meeting.js
+++ meeting_app/static/meeting.js
@@ -13,7 +13,7 @@ fetchData(user_id);
 async function fetchData(user_id) {
 
     try {
-        const response = await fetch(`http://77.232.128.211:8020/meeting/show/${user_id}`);
+        const response = await fetch(`http://localhost:8020/meeting/show/${user_id}`);
         const responseData = await response.json();
 
         console.log(responseData);
